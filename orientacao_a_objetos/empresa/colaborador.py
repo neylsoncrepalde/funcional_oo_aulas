b4afd75ee65eec3fec220ff7a45e1cb78b27fc66
Neylson Crepalde <neylsoncrepalde@gmail.com>
2020-06-20 14:49:29 -0300

    Create colaborador.py

---
+++ orientacao_a_objetos/empresa/colaborador.py
@@ -0,0 +1,11 @@
+class Colaborador:
+
+    def __init__(self, nome, setor):
+        self.nome = nome
+        self.setor = setor
+
+    def getNome(self):
+        print(self.nome)
+
+    def getSetor(self):
+        print(self.setor)
