4eef529526a2ff29d4e66142e7e056ddbda7e049
Neylson Crepalde <neylsoncrepalde@gmail.com>
2020-06-20 14:48:59 -0300

    Create main.py

---
+++ orientacao_a_objetos/cachorro/main.py
@@ -0,0 +1,15 @@
+from cachorro import Cachorro
+
+cao1 = Cachorro('Rex', 'Vira-lata', 3, 'Neylson')
+
+print(f'Ações do cachorrinho {cao1.nome}:')
+
+cao1.latir()
+cao1.largarOsso()
+cao1.latir()
+cao1.pegarOsso()
+cao1.latir()
+cao1.pegarOsso()
+cao1.darPatinha()
+cao1.largarOsso()
+cao1.latir()
